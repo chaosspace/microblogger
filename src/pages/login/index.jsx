2f581ba51b78024b0192bf523919aee6e704a609
hbc <940010004@qq.com>
2023-02-15 10:00:48 +0800

    个人终

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 
 import { Link } from "react-router-dom";
-// import { httpReq } from "../../tool/httpReq";
 import { httpReq } from "../../tool/httpReq";
 import loginCss from "./login.module.css";
 
